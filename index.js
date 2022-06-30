aec80831d8342f479bd0ad3e10336c019fee95e6
yeosiowvic <victoryeo1974@gmail.com>
2022-06-30 15:47:31 +0800

    adding string argument

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 let addon=require('./index.node');
-console.log(addon.hello);
+console.log(addon)
+addon.myFunc.toString();
+addon.myFunc("sent from me")
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,60 @@
-use nodejs_sys::{napi_create_string_utf8, napi_env, napi_set_named_property, napi_value};
+use nodejs_sys::{
+    napi_callback_info, napi_create_function, napi_env, napi_get_cb_info, napi_get_undefined,
+    napi_get_value_string_utf8, napi_set_named_property, napi_value,
+};
+
 use std::ffi::CString;
+
+pub unsafe extern "C" fn printme(env: napi_env, info: napi_callback_info) -> napi_value {
+// creating a buffer of arguments
+    let mut buffer: [napi_value; 1] = std::mem::MaybeUninit::zeroed().assume_init();
+    let mut argc = 1 as usize;
+// getting arguments
+    napi_get_cb_info(
+        env,
+        info,
+        &mut argc,
+        buffer.as_mut_ptr(),
+        std::ptr::null_mut(),
+        std::ptr::null_mut(),
+    );
+    let mut len = 0;
+// getting length by passing null buffer
+    napi_get_value_string_utf8(env, buffer[0], std::ptr::null_mut(), 0, &mut len);
+    let size = len as usize;
+// creating a buffer where string can be placed
+    let mut ve: Vec<u8> = Vec::with_capacity(size + 1);
+    let raw = ve.as_mut_ptr();
+// telling rust not manage the vector
+    std::mem::forget(ve);
+    let mut cap = 0;
+// getting the string value from napi_value
+    let _s = napi_get_value_string_utf8(env, buffer[0], raw as *mut i8, size + 1, &mut cap);
+    let s = String::from_raw_parts(raw, cap as usize, size);
+// printing the string
+    println!("{}", s);
+// creating an undefined
+    let mut und: napi_value = std::mem::zeroed();
+    napi_get_undefined(env, &mut und);
+// returning undefined
+    und
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn napi_register_module_v1(
     env: napi_env,
     exports: napi_value,
 ) -> nodejs_sys::napi_value {
-// creating a C string
-    let key = CString::new("hello").expect("CString::new failed");
-// creating a memory location where the pointer to napi_value will be saved
+    let p = CString::new("myFunc").expect("CString::new failed");
     let mut local: napi_value = std::mem::zeroed();
-// creating a C string
-    let value = CString::new("world!").expect("CString::new failed");
-// creating napi_value for the string
-    napi_create_string_utf8(env, value.as_ptr(), 6, &mut local);
-// setting the string on the exports object
-    napi_set_named_property(env, exports, key.as_ptr(), local);
-// returning the object 
+    napi_create_function(
+        env,
+        p.as_ptr(),
+        5,
+        Some(printme),
+        std::ptr::null_mut(),
+        &mut local,
+    );
+    napi_set_named_property(env, exports, p.as_ptr(), local);
     exports
 }
